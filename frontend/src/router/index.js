a4cef58dc32d43ee2c223ccce03ae4f9b8fe5a61
felixatschool <felix.lefebvre.1@ens.etsmtl.ca>
2024-01-06 23:41:37 -0500

    implement login form

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '../views/login.vue';
+import Home from '../views/home.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
     },
   ]
 })
--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,58 +1,104 @@
-<script>
-import { ref, onMounted } from 'vue';
-import { initializeApp } from 'firebase/app';
-import {
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  getAuth
-} from 'firebase/auth';
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { initializeApp } from 'firebase/app';
+  import {
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    getAuth
+  } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN
-};
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN
+  };
 
-export default {
-  setup() {
-    const message = ref('');
-    console.log(firebaseConfig);
+  const router = useRouter();
 
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app, {/* extra options */ });
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app, {});
 
-    onMounted(() => {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-	  console.log(user);
-          message.value = "Welcome, " + user.email;
-        } else {
-          message.value = "No user signed in.";
-        }
-      });
+  const email = ref('');
+  const password = ref('');
 
-      signIn();
-    });
-
-    function signIn() {
-      const email = "felix@app.com";
-      const password = "admin123";
-
-      signInWithEmailAndPassword(auth, email, password)
-        .catch((error) => {
-          message.value = error.message;
-        });
+  const signIn = async () => {
+    try{
+      await signInWithEmailAndPassword(auth, email.value, password.value)
+      router.push( { name: "Home" });
+    } catch (e) {
+      console.log(e);
     }
-
-    return {
-      message
-    };
   }
-};
 </script>
 
 <template>
-  <div>
-    <p id="message">{{ message }}</p>
+  <div class="login-container">
+    <div class="login-card">
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <label for="email">Email:</label>
+        <input type="text" v-model="email" id="email" required />
+
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" id="password" required />
+
+        <button @click="signIn()">Login</button>
+      </form>
+    </div>
   </div>
 </template>
 
+<style scoped>
+  .login-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: #f0f0f0;
+  }
+
+  .login-card {
+    max-width: 300px;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  h1 {
+    font-size: 24px;
+    text-align: center;
+    margin-bottom: 20px;
+    color:black;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    margin-bottom: 8px;
+    color:black;
+  }
+
+  input {
+    padding: 10px;
+    margin-bottom: 16px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
+
+  button {
+    background-color: #02559f;
+    color: #fff;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  button:hover {
+    background-color: #45a049;
+  }
+</style>
